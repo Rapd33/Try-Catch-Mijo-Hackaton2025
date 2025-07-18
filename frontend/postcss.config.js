9725c8554143c4d39565461be77b0b0e1effbd7c
Rapd33 <rafaelpatinodiaz@gmail.com>
2025-07-18 17:46:29 -0500

    Añade una dependencia faltante en el front y se configura para el buen funcionamiento y el corecto montaje del container front.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 export default {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {}, // importante para compatibilidad en navegadores
   },
 }
